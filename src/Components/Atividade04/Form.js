7770ae3a343a18800272e58eba83d0af6cad9985
vitorsalesi <114588382+vitorsalesi@users.noreply.github.com>
2023-06-09 15:11:49 -0300

    Update Form.js

--- src/Components/Atividade04/Form.js
+++ src/Components/Atividade04/Form.js
@@ -16,7 +16,7 @@ export default function Form() {
         <form onSubmit={handleSubmit}>
             <label>
                 To {' '}
-                <select value={to} onChange={(e) => setMessage(e.target.value)}>
+                <select value={to} onChange={(e) => setTo(e.target.value)}>
                     <option value="Alice">Alice</option>
                     <option value="Bob">Bob</option>
                 </select>
